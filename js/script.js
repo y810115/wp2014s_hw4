5895e0ecc0d9f62cfa33c5898c07ad62c49dbf2b
101104020_Chiayu <101104020@nccu.edu.tw>
2014-06-14 01:23:19 +0800

    change

--- js/script.js
+++ js/script.js
@@ -1,171 +1,138 @@
-/*
-這檔案協助你編寫JS，請注意每個code block的使用，若你對自己的javascript很有信心，或是你認為我寫的方式有bug，歡迎自行修改編排
-*/
+// JavaScript Document
+window.fbAsyncInit = function () {
+	FB.init({
+		appId: '1437646656493417', 
+		xfbml: true,
+		version: 'v2.0'
+	});
+	FB.getLoginStatus(function (response) {
+		if (response.status === 'connected') {
+			var uid = response.authResponse.userID;
+			var authToken = response.authResponse.accessToken;
+			window.e = authToken;
+			FB.api('/me', function (response) {
+				//console.log(response);
+				
+			});
+			FB.api('/me/picture?type=large', function(response) { // normal/large/squere 
+					 var str="<img src="+ response.data.url +">";
+					 //$('#preview1').append(str);
+					$('#preview1').attr("src",response.data.url); //頁面1顯示
+				});
+		} else if (response.status === 'not_authorized') {
+			console.log("this user is not authorizied your apps");			
+			FB.login(function (response) {
+				// FB.api('/me/feed', 'post', {message: 'I\'m started using FB API'});
+				if (response.authResponse) { // if user login to your apps right after handle an event
+				window.location.reload();
+				};
+			}, {
+				scope: 'user_location,user_photos'
+			});
+		} else {
+			console.log("this isn't logged in to Facebook.");
+			FB.login(function (response) {
+				if (response.authResponse) {
+					window.location.reload();
+				} else {
+				//alertify.alert('An Error has Occurs,Please Reload your Pages');
+				}
+				});
+			}
+		});
+		
+	var ctx = document.getElementById('canvas').getContext('2d'); 
+	ctx.font='20px "Arial"'; 
+	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); 
+    var img = new Image(); 
+    img.src = "img/overlay.png"; 
+	var img2 = new Image(); 
+	img2.src = "img/overlayback.png" 
+	var img3 = new Image();
+	img3.src = "img/typography.png"
+	
+    var canvas=document.getElementById("canvas"); 
+    var ctx=canvas.getContext("2d"); 
+    var canvasOffset=$("#canvas").offset();
+    var offsetX=canvasOffset.left;
+    var offsetY=canvasOffset.top;
+    var canvasWidth=canvas.width;
+    var canvasHeight=canvas.height;
+    var isDragging=false;
 
-//facebook init
-          window.fbAsyncInit = function () {
-            FB.init({
-                appId: ' 1437646656493417   ', 
-                xfbml: true,
-                version: 'v2.0'
-            });
-    
-//輸入基本的Facebook init的狀態，與Facebook 連接，包括APP ID的設定
-
-
-
-            FB.getLoginStatus(function (response) {
-                if (response.status === 'connected') {
-                    var uid = response.authResponse.userID;
-                    var accessToken = response.authResponse.accessToken;
-                     window.e = authToken;
-                       });                
-
-                    FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
-                        var str="<img src="+ response.data.url +">";
-                        $('#preview1').attr("src",response.data.url);
-                    });
-
-                } else if (response.status === 'not_authorized') {
-                    console.log("this user is not authorizied your apps");
-                    FB.login(function (response) {
-                        // FB.api('/me/feed', 'post', {message: 'I\'m started using FB API'});
-                        if (response.authResponse) { // if user login to your apps right after handle an event
-                            window.location.reload();
-                        };
-                    }, {
-                        scope: 'user_location,user_photos'
-                    });
-                } else {
-                    console.log("this isn't logged in to Facebook.");
-                    FB.login(function (response) {
-                        if (response.authResponse) {
-                            window.location.reload();
-                        } else {
-                            //alertify.alert('An Error has Occurs,Please Reload your Pages');
-                        }
-                    });
-                }
-            });
-
-
-//以下為canvas的程式碼，基本上不需多動，依據comments修改即可
-  
-  //起始畫面
-  var ctx = document.getElementById('canvas').getContext('2d'); //宣告變數找到頁面的canvas標籤的2d內容
-  ctx.font='20px "Arial"'; //設定字體與大小
-  ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
-    var img = new Image(); // 新增圖像1
-    img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
-  var img2 = new Image(); //新增圖像2
-  img2.src = "img/overlayback.jpg" //圖像路徑
-  var img3 = new Image();//新增圖像3
-  img3.src = "img/typography.png"//圖像路徑
-  
-  
-
-  //宣告基本變數
-    var canvas=document.getElementById("canvas"); //宣告變數找到canvas標籤
-    var ctx=canvas.getContext("2d"); //找到2d內容
-    var canvasOffset=$("#canvas").offset();//找到offset
-    var offsetX=canvasOffset.left;//左方
-    var offsetY=canvasOffset.top;//上方
-    var canvasWidth=canvas.width;//大小
-    var canvasHeight=canvas.height;//高度
-    var isDragging=false;//拖拉
-
-    function handleMouseDown(e){//滑鼠按下的函數
-      canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
-      canMouseY=parseInt(e.clientY-offsetY);//抓滑鼠游標y
-      // set the drag flag
-      isDragging=true;//宣告拖拉變數
+    function handleMouseDown(e){
+		canMouseX=parseInt(e.clientX-offsetX);
+		canMouseY=parseInt(e.clientY-offsetY);
+		isDragging=true;
     }
 
-    function handleMouseUp(e){//滑鼠放掉的函數
+    function handleMouseUp(e){
+		canMouseX=parseInt(e.clientX-offsetX);
+		canMouseY=parseInt(e.clientY-offsetY);
+		isDragging=false;
+    }
+
+    function handleMouseOut(e){
       canMouseX=parseInt(e.clientX-offsetX);
       canMouseY=parseInt(e.clientY-offsetY);
-      // clear the drag flag
-      isDragging=false;
     }
 
-    function handleMouseOut(e){//滑鼠移開的函數
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // user has left the canvas, so clear the drag flag
-      //isDragging=false;
+    function handleMouseMove(e){
+		canMouseX=parseInt(e.clientX-offsetX);
+		canMouseY=parseInt(e.clientY-offsetY);
+		if(isDragging){ 
+          	ctx.clearRect(0,0,canvasWidth,canvasHeight); 
+			var profileIMG = document.getElementById("preview1");
+			profileIMG.crossOrigin = "Anonymous";  
+			//canvas.width = profileIMG.width;
+			//canvas.height = profileIMG.height;
+			ctx.drawImage(profileIMG,0,0);
+			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); 
+			ctx.drawImage(img2,0,0); 
+			var inputedText = $('#inputed').val();
+			ctx.fillStyle = "black"; 
+			ctx.font='20px "微軟正黑體"'; 
+			ctx.fillText(inputedText, canMouseX-1/2,canMouseY-30/2); 
+		}
     }
-
-    function handleMouseMove(e){//滑鼠移動的event
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // if the drag flag is set, clear the canvas and draw the image
-      if(isDragging){ //當拖拉為True時
-            ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-      var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
-      profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
-      //canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
-      //canvas.height = profileIMG.height;
-      ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
-      ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-      ctx.drawImage(img2,0,0); //劃入img2
-      var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
-      ctx.fillStyle = "black"; //字體顏色
-      ctx.font='20px "微軟正黑體"'; //字體大小和字形
-      ctx.fillText(inputedText, canMouseX-1/2,canMouseY-30/2); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
-      }
-    }
-
-  //抓取滑鼠移動的event
     $("#canvas").mousedown(function(e){handleMouseDown(e);});
     $("#canvas").mousemove(function(e){handleMouseMove(e);});
     $("#canvas").mouseup(function(e){handleMouseUp(e);});
     $("#canvas").mouseout(function(e){handleMouseOut(e);});
+	}; //<<<<<<<<<<<<<<<init end
 
-
-//可以思考這程式要放在init內還是init外?
-
-
-
-
-// }; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<init end
-
-
-
-   
-
-
-
-
-//LOAD FACEBOOK SDK ASYNC，這是基本的東西，應該不用多說了吧
 (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js"; 
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
-
-// Post a BASE64 Encoded PNG Image to facebook，以下程式為把照片po到facebook的方法，基本上這樣就可以不用動了，但思考authToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
+	var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {
+			return;
+		}
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+		
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=320206518133001&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	
 function PostImageToFacebook(authToken) {
-  $('.info').append('<img src="img/loading.gif"/>')//載入loading的img
-    var canvas = document.getElementById("canvas");//找canvas
-    var imageData = canvas.toDataURL("image/png");//把canvas轉換PNG
+	$('.info').append('<img src="img/loading.gif"/>')
+    var canvas = document.getElementById("canvas");
+    var imageData = canvas.toDataURL("image/png");
     try {
-        blob = dataURItoBlob(imageData);//把影像載入轉換函數
+        blob = dataURItoBlob(imageData);
     } catch (e) {
-        console.log(e);//錯誤訊息的log
+        console.log(e);
     }
     var fd = new FormData();
     fd.append("access_token", authToken);//請思考accesstoken要怎麼傳到這function內
-    fd.append("source", blob);//輸入的照片
-    fd.append("message", "這是HTML5 canvas和Facebook API結合教學");//輸入的訊息
+    fd.append("source", blob);
+    fd.append("message", "這是HTML5 canvas和Facebook API結合教學");
     try {
         $.ajax({
             url: "https://graph.facebook.com/me/photos?access_token=" + authToken,//GraphAPI Call
@@ -191,9 +158,6 @@ function PostImageToFacebook(authToken) {
     }
 }
 
-
-
-
 // Convert a data URI to blob把影像載入轉換函數
 function dataURItoBlob(dataURI) {
     var byteString = atob(dataURI.split(',')[1]);
@@ -206,7 +170,4 @@ function dataURItoBlob(dataURI) {
         type: 'image/png'
     });
 }
-
-
-
 
